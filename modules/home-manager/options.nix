754ce5ff94b1fc917f1a628ce624a569a6ba6c83
Mads Johannessen <johannessen.mads@gmail.com>
2026-07-30 21:59:11 +0200

    feat: add module options (theme selector + per-app toggles)

---
+++ modules/home-manager/options.nix
@@ -0,0 +1,30 @@
+{ lib, themes }:
+
+let
+  themeNames = builtins.attrNames themes;
+  appNames = [
+    "alacritty" "btop" "chromium" "foot" "ghostty" "gum" "helix"
+    "hyprland" "hyprlock" "keyboard-rgb" "kitty" "mako" "obsidian"
+    "quickshell" "swayosd" "walker" "waybar"
+  ];
+  mkAppOption = name: {
+    ${name} = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Nyx theming for ${name}";
+      };
+    };
+  };
+in
+{
+  options.nyx = {
+    enable = lib.mkEnableOption "Nyx theme module";
+
+    theme = lib.mkOption {
+      type = lib.types.enum themeNames;
+      default = "gruvbox";
+      description = "Active Nyx theme";
+    };
+  } // lib.foldl' (acc: name: acc // mkAppOption name) {} appNames;
+}
