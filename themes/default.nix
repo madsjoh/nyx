0c2865c9e912d0e23709770b12c43e77a46cf973
Mads Johannessen <johannessen.mads@gmail.com>
2026-07-30 21:57:59 +0200

    feat: add all 21 omarchy theme color definitions

---
+++ themes/default.nix
@@ -0,0 +1,23 @@
+{
+  catppuccin = import ./catppuccin.nix;
+  catppuccin-latte = import ./catppuccin-latte.nix;
+  ethereal = import ./ethereal.nix;
+  everforest = import ./everforest.nix;
+  flexoki-light = import ./flexoki-light.nix;
+  gruvbox = import ./gruvbox.nix;
+  hackerman = import ./hackerman.nix;
+  kanagawa = import ./kanagawa.nix;
+  last-horizon = import ./last-horizon.nix;
+  lumon = import ./lumon.nix;
+  matte-black = import ./matte-black.nix;
+  miasma = import ./miasma.nix;
+  nord = import ./nord.nix;
+  osaka-jade = import ./osaka-jade.nix;
+  retro-82 = import ./retro-82.nix;
+  ristretto = import ./ristretto.nix;
+  rose-pine = import ./rose-pine.nix;
+  solitude = import ./solitude.nix;
+  tokyo-night = import ./tokyo-night.nix;
+  vantablack = import ./vantablack.nix;
+  white = import ./white.nix;
+}
---
+++ themes/solitude.nix
@@ -0,0 +1,28 @@
+{
+  name = "solitude";
+  colors = {
+    accent = "#798186";
+    cursor = "#cacccc";
+    foreground = "#cacccc";
+    background = "#101315";
+    selection_foreground = "#101315";
+    selection_background = "#798186";
+    color0 = "#101315";
+    color1 = "#565d60";
+    color2 = "#9fa5a9";
+    color3 = "#d9dbdc";
+    color4 = "#798186";
+    color5 = "#aeaeae";
+    color6 = "#707070";
+    color7 = "#cbc2be";
+    color8 = "#4b4e55";
+    color9 = "#de6145";
+    color10 = "#343d41";
+    color11 = "#c9c2b4";
+    color12 = "#5d6367";
+    color13 = "#9a9a9a";
+    color14 = "#707070";
+    color15 = "#a5aeb4";
+  };
+  overrides = {};
+}
