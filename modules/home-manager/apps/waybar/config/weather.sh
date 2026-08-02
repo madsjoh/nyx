09af30a924fe0f6cf71c0739e419071fae597fb0
Mads Johannessen <johannessen.mads@gmail.com>
2026-08-02 19:56:03 +0200

    fix(waybar): use wttr.in format %l:+%c+%t with location in tooltip

--- modules/home-manager/apps/waybar/config/weather.sh
+++ modules/home-manager/apps/waybar/config/weather.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # Simple weather script using wttr.in
-weather=$(curl -s "wttr.in/?format=%c+%t" 2>/dev/null)
+weather=$(curl -s "wttr.in/?format=%l:+%c+%t" 2>/dev/null)
 
 if [[ -n $weather ]]; then
-  weather_icon=$(echo "$weather" | awk '{print $1}')
-  weather_temp=$(echo "$weather" | awk '{print $2}')
+  # Extract icon and temp from "City: icon temp" format
+  after_colon=$(echo "$weather" | sed 's/^.*: //')
+  weather_icon=$(echo "$after_colon" | awk '{print $1}')
+  weather_temp=$(echo "$after_colon" | awk '{print $2}')
   printf '{"text":"%s %s","tooltip":"%s"}\n' "$weather_icon" "$weather_temp" "$weather"
 else
   printf '{"text":"","class":"unavailable"}\n'
