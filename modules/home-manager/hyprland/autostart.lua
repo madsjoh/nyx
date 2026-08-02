fe1fba9f2ff185f1a756f4262fbe83e7a2373dbe
Mads Johannessen <johannessen.mads@gmail.com>
2026-08-02 16:45:18 +0200

    fix: kill stale swaybg before starting new one

--- modules/home-manager/hyprland/autostart.lua
+++ modules/home-manager/hyprland/autostart.lua
@@ -2,7 +2,7 @@ o.launch_on_start("hypridle")
 o.launch_on_start("mako")
 o.launch_on_start("waybar")
 o.launch_on_start("fcitx5 --disable notificationitem")
-o.exec_on_start("swaybg -i ~/.local/state/nyx/current/background -m fill")
+o.exec_on_start("pkill swaybg 2>/dev/null; swaybg -i ~/.local/state/nyx/current/background -m fill")
 o.exec_on_start("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 
 o.exec_on_start("systemctl --user import-environment $(env | cut -d'=' -f 1)")
