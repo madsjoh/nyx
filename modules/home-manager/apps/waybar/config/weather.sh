ef8d50c9fdf16274b6db751650a3d833ef344923
Mads Johannessen <johannessen.mads@gmail.com>
2026-08-02 19:42:20 +0200

    fix(waybar): show temperature alongside weather icon

--- modules/home-manager/apps/waybar/config/weather.sh
+++ modules/home-manager/apps/waybar/config/weather.sh
@@ -5,7 +5,8 @@ weather=$(curl -s "wttr.in/?format=%c+%t" 2>/dev/null)
 
 if [[ -n $weather ]]; then
   weather_icon=$(echo "$weather" | awk '{print $1}')
-  printf '{"text":"%s","tooltip":"%s"}\n' "$weather_icon" "$weather"
+  weather_temp=$(echo "$weather" | awk '{print $2}')
+  printf '{"text":"%s %s","tooltip":"%s"}\n' "$weather_icon" "$weather_temp" "$weather"
 else
   printf '{"text":"","class":"unavailable"}\n'
 fi
